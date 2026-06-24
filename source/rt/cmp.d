85159066d51908824396d720d1a14636c5235990
LunaTheFoxgirl <luna@foxgirls.gay>
2026-06-24 21:16:26 +0200

    fix __cmp collission

--- source/rt/cmp.d
+++ source/rt/cmp.d
@@ -158,12 +158,14 @@ int __cmp(T1, T2)(T1[] s1, T2[] s2) @nogc
     }
     return (s1.length > s2.length) - (s1.length < s2.length);
 }
-int __cmp(T)(scope const T lhs, scope const T rhs) @trusted @nogc nothrow {
+
+int __cmp(T)(scope const T lhs, scope const T rhs) @trusted @nogc nothrow
+if (!is(T == U[], U)) {
     static if (is(T.opCmp))
         return lhs.opCmp(rhs);
     else {
-        if (lhs < rhs) return -1;
-        else if (lhs > rhs) return 1;
+        if (cast(void*)lhs < cast(void*)rhs) return -1;
+        else if (cast(void*)lhs > cast(void*)rhs) return 1;
         else return 0;
     }
 }
